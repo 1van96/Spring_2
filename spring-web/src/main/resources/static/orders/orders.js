7635725491686b1ea1362d7746f4b7e6bff3e444
1van96 <40235313+1van96@users.noreply.github.com>
2022-01-20 18:32:07 +0300

    lesson_3

--- spring-web/src/main/resources/static/orders/orders.js
+++ spring-web/src/main/resources/static/orders/orders.js
@@ -1,7 +1,7 @@
-angular.module('market-front').controller('ordersController', function ($scope, $http, $location, $localStorage) {
+angular.module('market-front').controller('ordersController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/app/';
 
-   $scope.loadOrders = function () {
+    $scope.loadOrders = function () {
         $http.get(contextPath + 'api/v1/orders')
             .then(function (response) {
                 $scope.MyOrders = response.data;
