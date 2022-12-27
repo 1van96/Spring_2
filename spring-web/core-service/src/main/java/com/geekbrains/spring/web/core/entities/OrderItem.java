8aaae52f621cc1ebbfc33d68c2f20a2e19c98935
1van96 <40235313+1van96@users.noreply.github.com>
2022-12-27 13:01:33 +0300

    Добавлен билдер

--- spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/entities/OrderItem.java
+++ spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.geekbrains.spring.web.core.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "order_items")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItem {
